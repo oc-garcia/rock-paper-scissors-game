3ad3b9c5af10074743e6f152f4722882f69f90f0
oc-garcia <octaviogarcia1988@gmail.com>
2023-05-08 10:33:00 -0300

    Adding props to scorecard

--- src/Components/ScoreBoard/ScoreBoard.tsx
+++ src/Components/ScoreBoard/ScoreBoard.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import logoClassic from "./logo.svg";
 import styles from "./ScoreBoard.module.css";
 
-type Props = {};
+type Props = {
+  score: number;
+};
 
-export default function ScoreBoard({}: Props) {
+export default function ScoreBoard({score}: Props) {
   return (
     <header className={styles.headerContainer}>
       <img src={logoClassic} alt="logo" className={styles.logo} />
       <div className={styles.scoreContainer}>
         <h1>Score</h1>
-        <p>12</p>
+        <p>{score}</p>
       </div>
     </header>
   );
