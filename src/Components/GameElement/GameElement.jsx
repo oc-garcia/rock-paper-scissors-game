3c387301bc67d791f33233afe853a30fd59d1f4c
oc-garcia <octaviogarcia1988@gmail.com>
2023-05-15 19:02:18 -0300

    Created game element component

---
+++ src/Components/GameElement/GameElement.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function GameElement() {
+  return (
+    <section>
+        <div>
+            <img src='' alt=''/>
+        </div>
+    </section>
+  )
+}
